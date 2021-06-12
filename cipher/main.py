5096300327bb094988914f6c93ad02bd121d1004
HGARgG-0710 <kuznetzovigor2007@gmail.com>
2021-06-12 22:44:39 +0300

    Adding the program for ciphering passwords or any other strings. It works this way: you pass three arguments to it ( they are: 0. input file with all the ciphering words, 1. output file for outputing the ciphered string and 2. string to be ciphered). After you run it, you get a ciphered string in the out.txt or any other file that was specified as the one for output.

---
+++ cipher/main.py
@@ -0,0 +1,29 @@
+from random import choice
+from sys import argv
+
+
+def cipher(password: str, cipher_keywords: list):
+    ciphered = ""
+
+    for char in password:
+        ciphered += char + choice(cipher_keywords)
+
+    return ciphered
+
+
+def main(arguments: list):
+    args = arguments.copy()
+    words = []
+    
+    for i in range(1, len(args)):
+        args[i] = args[i].split("=")[1].strip()
+
+    with open(args[1], "r") as readfile:
+        for line in readfile:
+            words += [line.strip()]
+
+    with open(args[2], "w") as writefile:
+        writefile.write(cipher(args[3], words))
+
+
+main(argv)
